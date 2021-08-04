047c70480b01052995541dc503ba27c4c4bbc702
ZBX1203 <64179870+ZBX1203@users.noreply.github.com>
2021-08-04 09:19:53 +0800

    Add files via upload

--- MSET.py
+++ MSET.py
@@ -1,10 +1,7 @@
-import numpy as np
-from scipy import io
+from DPC import *
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-step=100
-delta=0.001
 
 #计算保存记忆矩阵的Temp矩阵
 def Temp_MemMat(memorymat,Temp_name):
@@ -28,7 +25,7 @@ def MSET(memorymat,Kobs,Temp_name):#Temp为临时计算的矩阵
     Kest=Kest.T
     return Kest
 
-#判断输入的观测向量，再传到相应记忆矩阵中，得到估计值
+#判断输入的观测向量，再传到相应记忆矩阵中，得到估计值，调用MSET
 def MSETs(memorymat1_name,memorymat2_name,memorymat3_name,Kobs):
     row_Kobs=Kobs.shape[0]
     col_Kobs = Kobs.shape[1]
@@ -55,9 +52,7 @@ def Cal_sim(Kobs,Kest):
     return sim
 
 #各变量及其误差的可视化
-def pic_vars(label,Kobs,Kest,np_Dmax,np_Dmin):
-    Kobs = Kobs * (np_Dmax - np_Dmin) + np_Dmin  # 反归一化
-    Kest = Kest * (np_Dmax - np_Dmin) + np_Dmin  # 反归一化
+def pic_vars(label,Kobs,Kest):
     col_num=Kobs.shape[1]
     e=np.ones((Kobs.shape[0],Kobs.shape[1]))
     plt.ion()
@@ -69,12 +64,14 @@ def pic_vars(label,Kobs,Kest,np_Dmax,np_Dmin):
         plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f'))
         plt.plot(Kobs[:, i], 'steelblue', label='观测值', lw=1.5)
         plt.plot(Kest[:, i], 'indianred', label='估计值', lw=1.5)
+        #fontsize的值为为数字时可调节字体大小，也可以填写’small’，‘large’，‘medium’，默认为’large’
         plt.legend(loc='upper right', fontsize=13)
-        plt.xlabel('样本序号', fontsize=20)
-        plt.ylabel(label[i], fontsize=20, verticalalignment='bottom')
+        plt.xlabel('样本序号', fontsize=13)
+        #参数verticalalignment的值为’top’, ‘bottom’, ‘center’,‘baseline’，意思为上下平移向figure与axis之间的中间线对齐
+        plt.ylabel(label[i], fontsize=13, verticalalignment='bottom')
         plt.xticks(fontsize=20)
         plt.yticks(fontsize=20)
-        plt.subplot(212)
+        plt.subplot(212)#plt.subplot(222)表示将整个图像窗口分为2行1列, 当前位置为2.
         plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f'))
         e[:, i] = (np.abs(Kobs[:, i] - Kest[:, i]) / Kobs[:, i]) * 100
         plt.plot(e[:, i], 'peru', lw=1)  # 偏离度
@@ -82,66 +79,10 @@ def pic_vars(label,Kobs,Kest,np_Dmax,np_Dmin):
         plt.ylabel('相对误差/%', fontsize=20)
         plt.xticks(fontsize=20)
         plt.yticks(fontsize=18)
+        plt.ioff()
         plt.show()
     np.set_printoptions(formatter={'float': '{: 0.4f}'.format})
 
-#误差贡献率
-def error_contribution(Kobs,Kest,momtent,label):
-    error=(Kobs - Kest)**2
-    error_cont =[]
-    for row in error:
-        error_cont.append(row/row.sum())
-    plt.ion()
-    plt.rcParams['font.sans-serif'] = ['SimHei']  # 图片显示中文
-    plt.rcParams['axes.unicode_minus'] = False
-    plt.bar(np.arange(1,Kobs.shape[1]+1,1), error_cont[momtent])
-    plt.xticks(range(1, Kobs.shape[1]+1, 1), label, rotation=80)
-    plt.title('1min内第%d时刻各变量的误差贡献率'%(momtent+1))
-    plt.show()
-
-# 累计误差贡献率
-def Accumu_errorContirbution(Kobs,Kest,momtent,time_range,label):
-    if time_range==0:
-        print('Warning:time_range cannot be zero')
-        return
-    else:
-        error = (Kobs - Kest) ** 2
-        error_cont = np.zeros((1,Kobs.shape[1]))
-        for i in range(time_range):
-            error_cont += error[momtent+i]/error[momtent+i].sum()
-        error_cont = np.squeeze(error_cont/time_range)
-        plt.ion()
-        plt.rcParams['font.sans-serif'] = ['SimHei']  # 图片显示中文
-        plt.rcParams['axes.unicode_minus'] = False
-        plt.bar(np.arange(1,Kobs.shape[1]+1,1) ,error_cont)
-        plt.xticks(range(1,Kobs.shape[1]+1,1),label,rotation=80)
-        plt.title('1min内第%d个时刻发出预警起的累计误差贡献率' % (momtent+1))
-        plt.show()
-
-#更新记忆矩阵，暂未用
-def Mat_update(Kobs,sim,thres,memorymat_name,Temp_name):
-    Kobs_row=Kobs.shape[0]
-    Kobs_col = Kobs.shape[1]
-    k_index=np.arange(201,301,1)
-    break_flag=False
-    mat_temp = []
-    for i in range(Kobs_row):
-        if sim[i]>thres :#判断观测向量是否正常
-            for k,k_in in enumerate(k_index):
-                for j in range(Kobs_col):
-                    if np.abs(Kobs[i,j] - k_in * (1 / step)) < delta:
-                        mat_temp.append(Kobs[i])
-                        print('add state')
-                        break_flag=True
-                        break
-                if break_flag==True:
-                    break
-    mat_temp= np.array(mat_temp, dtype=float)
-    print('size of mat_temp:',mat_temp.shape)
-    # Temp_MemMat(memorymat,Temp_name)
-    # print('size of memorymat',memorymat.shape)
-    # np.save(memorymat_name,memorymat)
-    return mat_temp
 
 
 
